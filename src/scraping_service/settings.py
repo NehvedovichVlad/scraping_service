5ab8dd92f0a3b0fd4b3d0fa479cbc4971ea62973
Uladzislau Nekhviadovich <vladislavnehvedovich@gmail.com>
2020-11-03 18:48:03 +0300

    mistake2

--- src/scraping_service/settings.py
+++ src/scraping_service/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*ue0ruo66w%6vu10klckl0#htix6^2(^+0t3v$7)c-+o#$w+s^'
+SECRET_KEY = 'n_(immd-7sjb0hb!rn%-gp6vep))0kv#zew6#=v@4cbn=g_(&i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
