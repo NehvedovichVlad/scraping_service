61305f4bac1bc96750eaa4e794af41ba6fdf51b4
Uladzislau Nekhviadovich <vladislavnehvedovich@gmail.com>
2020-11-18 21:47:57 +0300

    delete user and save change user

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,9 +1,10 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
 
 from accounts.forms import UserLoginForm, UserRegisterForm, UserUpdateForm
 
+User = get_user_model()
 
 def login_view(request):
     if request.method == 'POST':
@@ -43,15 +44,32 @@ def register_view(request):
 
 
 def update_view(request):
+    """checks if the user is registered"""
     if request.user.is_authenticated:
         user = request.user
         if request.method == 'POST':
             form = UserUpdateForm(request.POST)
-        else:
-            form = UserUpdateForm(
-                initial={'city': user.city, 'language': user.language, 
-                         'send_email': user.send_email}
-            )
-            return render(request, 'accounts/update.html', {'form': form})
+            if form.is_valid():
+                data = form.cleaned_data
+                user.city = data['city']
+                user.language = data['language']
+                user.send_email = data['send_email']
+                user.save()
+                return redirect('home')
+            
+        form = UserUpdateForm(
+            initial={'city': user.city, 'language': user.language, 
+                     'send_email': user.send_email}
+        )
+        return render(request, 'accounts/update.html', {'form': form})
     else:
         return redirect('login')
+
+
+def delete_view(request):
+    if request.user.is_authenticated:
+        user = request.user
+        if request.method == 'POST':
+            qs = User.objects.get(pk=user.pk)
+            qs.delete()
+    return redirect('home')
