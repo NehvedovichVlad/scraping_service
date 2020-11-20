62cc30933a5352ad9ad986de73a7c573e135d1b0
Uladzislau Nekhviadovich <vladislavnehvedovich@gmail.com>
2020-11-20 18:15:24 +0300

    create vacancies for the user

--- src/send_emails.py
+++ src/send_emails.py
@@ -12,7 +12,7 @@ from scraping.models import Vacancy
 
 
 User = get_user_model()
-qs = User.objects.filter(send_mail=True).values('city', 'language', 'email')
+qs = User.objects.filter(send_email=True).values('city', 'language', 'email')
 users_dct = {}
 for i in qs:
     users_dct.setdefault((i['city'], i['language']), [])
@@ -22,4 +22,18 @@ if users_dct:
     for pair in users_dct.keys():
         params['city_id__in'].append(pair[0])
         params['language_id__in'].append(pair[1])
-    qs = Vacancy.objects.filter(**params)[:10]
+    qs = Vacancy.objects.filter(**params).values()[:10]
+    vacancies = {}
+    for i in qs:
+        vacancies.setdefault((i['city_id'], i['language_id']), [])
+        vacancies[(i['city_id'], i['language_id'])].append(i)
+    for keys, emails in users_dct.items():
+        rows = vacancies.get(keys, [])
+        html = ''
+        for row in rows:
+            html += f'<h5><a href="{row["url"]}">{row["title"]}</a></h5>'
+            html += f'<p>{row["description"]}</p>'
+            html += f'<p>{row["company"]}</p><br><hr>'
+
+
+
