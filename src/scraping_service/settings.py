0362e64bc7d231f40610e866fd53d1be8b450ff5
Uladzislau Nekhviadovich <vladislavnehvedovich@gmail.com>
2020-11-22 22:57:30 +0300

    password send_email

--- src/scraping_service/settings.py
+++ src/scraping_service/settings.py
@@ -7,7 +7,7 @@ from django.core.mail.backends import smtp
 from dotenv import load_dotenv
 load_dotenv()
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD ")
+EMAIL_HOST_PASSWORD = os.getenv(EMAIL_HOST_PASSWORD)
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -97,8 +97,8 @@ AUTH_USER_MODEL = 'accounts.MyUser'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv("EMAIL_HOST ")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_USE_TLS = True
-EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_PORT = 587
 EMAIL_HOST_USER = EMAIL_HOST_USER
 EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
