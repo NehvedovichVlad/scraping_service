1a0f1653988a24f9269c0d6c5215cc80aad06252
Uladzislau Nekhviadovich <vladislavnehvedovich@gmail.com>
2020-12-02 22:10:06 +0300

    .

--- src/scraping_service/wsgi.py
+++ src/scraping_service/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scraping_service.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scraping_service.settings.production')
 
 
 application = get_wsgi_application()
